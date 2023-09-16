636b084466c26c2f65bd7377b312deee6010c901
joelittyab-git <joeittab@gmail.com>
2023-09-16 19:44:58 +0530

    url routing

--- server/User/urls.py
+++ server/User/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
 from . import views
+
+'''
+     -Authentication:
+          [DESCRIPTION]:URL endpoints to authenticate user according to the credentials provided
+          [POST]:{username:..., password:...} -> returns 
+               :response:
+                    {"auth_status":"success","auth_data":{"auth_token":"Token ..."}}} -> authentication status successfull
+                    {"auth_status":"exception""info":{...}}} -> database integrity error / other exceptions
+                    {"auth_status":"denied"} -> authentication failed, onvalid credentials provided
+'''
 urlpatterns = [
     path('auth/', views.AuthenticationView.as_view())
 ]
