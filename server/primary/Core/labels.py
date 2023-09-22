af04a6e79e7d86248cf1a15006b75bb445c78c6b
joelittyab-git <joeittab@gmail.com>
2023-09-22 23:11:09 +0530

    deletes

--- server/primary/Core/labels.py
+++
@@ -1,5 +0,0 @@
-class Database:
-     PRIMARY_DATABASE = 'auth_primary'
-     USERS_DATABASE = 'auth_primary'
-     WEBSOCKET_DATABASE = 'channels_postgres'
-     PASSWORD_MANAGER_DATABASE = ''
--- server/primary/UserApp/urls.py
+++
@@ -1,6 +0,0 @@
-from . import views
-from django.urls import path
-urlpatterns = [
-     path('auth/', views.AuthenticationView.as_view()),
-     path('get/', views.RegistrationView.as_view())
-]
