14ebfe69f8ee3e8278064097d4b6fb5e868b6a85
joelittyab-git <joeittab@gmail.com>
2023-09-24 01:40:51 +0530

    added AuthenticationMiddleware.java -- auth middleware

---
+++ servers/primary/src/main/java/com/lambda/primary/Middlewares/AuthenticationMiddleware.java
@@ -0,0 +1,67 @@
+package com.lambda.primary.Middlewares;
+
+
+import com.lambda.primary.Objects.Http.HttpUtils;
+import com.lambda.primary.Objects.Http.MutableHttpServletRequest;
+import com.lambda.primary.Objects.User.AnonymousUser;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+
+@Component
+@Order(2)
+public class AuthenticationMiddleware implements Filter {
+
+    @Value("${application.config.auth.keyword}")
+    private String authKeyword;
+
+    /**
+     *
+     * @param servletRequest :
+     * @param servletResponse
+     * @param filterChain
+     * @throws IOException
+     * @throws ServletException
+     *
+     * Method to add to the header the user information using mutable http object
+     */
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        //extracting the protocol type
+        String requestProtocol = servletRequest.getProtocol();
+
+        if(requestProtocol.equalsIgnoreCase("HTTP/1.1")){
+            MutableHttpServletRequest httpRequest = new MutableHttpServletRequest((HttpServletRequest) servletRequest);
+
+            //extracting the header from the header list
+            String authHeader = httpRequest.getHeader("Authorization");
+            if(authHeader==null){
+                AnonymousUser anonymousUser = new AnonymousUser(HttpUtils.getRequestIP(httpRequest));
+                httpRequest.setHeader("user",anonymousUser);
+                filterChain.doFilter(
+                        httpRequest,
+                        servletResponse
+                );
+                return;
+            }
+            String authToken = authHeader.toLowerCase()
+                    .trim().replaceFirst(authKeyword.toLowerCase(),"").trim();
+
+            //extracting user associated with the token from database
+            /*
+                To be implemented
+            */
+            System.out.println(authToken);
+
+        }
+
+        //follows the execution
+         filterChain.doFilter(
+                servletRequest,
+                servletResponse
+        );
+    }
+}
