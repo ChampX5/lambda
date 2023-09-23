147597c8e39ba442fc3661ea831987d11e173b2d
joelittyab-git <joeittab@gmail.com>
2023-09-24 01:39:08 +0530

    added HttpServlet wrapper class for mutability

---
+++ servers/primary/src/main/java/com/lambda/primary/Objects/Http/MutableHttpServletRequest.java
@@ -0,0 +1,74 @@
+package com.lambda.primary.Objects.Http;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+import org.apache.catalina.LifecycleState;
+
+import java.util.*;
+
+public class MutableHttpServletRequest extends HttpServletRequestWrapper {
+
+    private final Map<String, Object> customHeaders;
+    public MutableHttpServletRequest(HttpServletRequest request) {
+        super(request);
+        customHeaders = new HashMap<>();
+    }
+
+    public void setHeader(String key, Object value){
+        customHeaders.put(key, value);
+    }
+
+
+    public Object getHeaderObject(String name) {
+        Object header = customHeaders.get(name);
+
+        if(!Objects.isNull(header)){
+            return header;
+        }
+
+        return super.getHeader(name);
+    }
+
+
+    @Override
+    public String getHeader(String name) {
+        try{
+            String  header = customHeaders.get(name).toString();
+
+            if(header!=null){
+                return header;
+            }
+        }catch (Exception ignore){}
+
+        return super.getHeader(name);
+    }
+
+    @Override
+    public Enumeration<String> getHeaderNames() {
+        Set<String> headerNames = customHeaders.keySet();
+        List<String> headerList = new ArrayList<>(headerNames);
+
+        Enumeration<String> headerEnumeration = super.getHeaderNames();
+
+        while(headerEnumeration.hasMoreElements()){
+            headerList.add(headerEnumeration.nextElement());
+        }
+
+        return Collections.enumeration(headerList);
+
+    }
+
+    @Override
+    public int getIntHeader(String name) {
+
+        Integer headerInt;
+
+        try{
+            headerInt = (Integer)(customHeaders.get(name));
+        }catch (Exception ignore){
+            return 0;
+        }
+
+        return headerInt;
+    }
+}
