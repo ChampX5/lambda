7e82cbcdda5e84121c7df7acd2c3d34d63532c06
joelittyab-git <joeittab@gmail.com>
2023-09-23 20:48:06 +0530

    spring init

---
+++ servers/primary/src/main/java/com/lambda/primary/PrimaryApplication.java
@@ -0,0 +1,13 @@
+package com.lambda.primary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimaryApplication.class, args);
+	}
+
+}
---
+++ servers/primary/src/test/java/com/lambda/primary/PrimaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambda.primary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
