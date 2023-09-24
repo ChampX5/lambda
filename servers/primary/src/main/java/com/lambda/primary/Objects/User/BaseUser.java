a87022527c5a573e6866d36b6f07df55f37e7adb
joelittyab-git <joeittab@gmail.com>
2023-09-25 00:54:35 +0530

    added base user class

--- servers/primary/src/main/java/com/lambda/primary/Objects/User/BaseUser.java
+++ servers/primary/src/main/java/com/lambda/primary/Objects/User/BaseUser.java
@@ -1,5 +1,30 @@
 package com.lambda.primary.Objects.User;
 
-public interface BaseUser {
-    boolean isAuthenticated();
+import lombok.Getter;
+
+@Getter
+public class BaseUser implements BaseUserInterface{
+
+    private final int userId;
+    private final String username;
+    private final String email;
+
+
+    public BaseUser(int userId, String username, String email) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return userId > 0;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseUser";
+    }
+
+
 }
