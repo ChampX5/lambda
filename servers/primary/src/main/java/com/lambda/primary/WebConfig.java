3e67f20134eda52f03964ad0226f94983eb26889
joelittyab-git <joeittab@gmail.com>
2023-09-25 00:52:13 +0530

    added webmvc configuration for cors mapping and converters

---
+++ servers/primary/src/main/java/com/lambda/primary/WebConfig.java
@@ -0,0 +1,21 @@
+package com.lambda.primary;
+
+import com.lambda.primary.Objects.User.UserConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new UserConverter());
+    }
+
+    //public cors mapping
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://loclahost:3000");
+    }
+}
