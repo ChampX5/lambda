711f86d55849e6bd403f34bc5d22c11b4e018862
joelittyab-git <joeittab@gmail.com>
2023-09-26 02:49:12 +0530

    User converter implementation

--- servers/primary/src/main/java/com/lambda/primary/Objects/User/UserConverter.java
+++ servers/primary/src/main/java/com/lambda/primary/Objects/User/UserConverter.java
@@ -1,19 +1,30 @@
 package com.lambda.primary.Objects.User;
 
 
+import com.lambda.primary.CoreExports.entities.User;
+import com.lambda.primary.Services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 
 public class UserConverter implements Converter<String, BaseUser> {
+
+    @Autowired
+    UserServices userServices;
+
     @Override
     public BaseUser convert(String value) {
         if(value.equalsIgnoreCase("-1")){
             return new AnonymousUser();
         }
+
+        User user =  userServices.fetchUserOnUsername(value);
+
         return new BaseUser(
-                1,
-                "admin",
-                "admin@email.com"
-        );
+                user.getUserId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getFirst_name(),
+                user.getLast_name());
     }
 
 
