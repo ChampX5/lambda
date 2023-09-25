659119c463c4f27d843e1538e2336114c5371559
joelittyab-git <joeittab@gmail.com>
2023-09-26 02:43:55 +0530

    security configuration

---
+++ servers/primary/src/main/java/com/lambda/primary/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.lambda.primary;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();  //deprecated
+        return http.build();
+    }
+}
