daf300b395fae1f322088ea1d9bdb69ea664242f
joelittyab-git <joeittab@gmail.com>
2023-09-26 02:40:28 +0530

    entity model for generating user auth tokens

---
+++ servers/primary/src/main/java/com/lambda/primary/CoreExports/entities/AuthToken.java
@@ -0,0 +1,39 @@
+package com.lambda.primary.CoreExports.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthToken {
+
+    @Id
+    private Long Id;
+
+
+    @OneToOne(targetEntity = User.class,cascade = {CascadeType.ALL})
+    @PrimaryKeyJoinColumn(referencedColumnName = "UserId")
+    private User user;
+
+    @Column(
+            columnDefinition = "VARCHAR(100)"
+    )
+    private String token;
+
+    @Column(
+            columnDefinition = "DATETIME"
+    )
+    private LocalDateTime loggedIn;
+
+
+
+}
