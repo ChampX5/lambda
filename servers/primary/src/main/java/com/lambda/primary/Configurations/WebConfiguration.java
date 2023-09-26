80d9ff2c7006940c95d1e783d372da878ab310e3
joelittyab-git <joeittab@gmail.com>
2023-09-26 21:34:23 +0530

    dependency injections for web conversion services

--- servers/primary/src/main/java/com/lambda/primary/Configurations/WebConfiguration.java
+++ servers/primary/src/main/java/com/lambda/primary/Configurations/WebConfiguration.java
@@ -1,6 +1,10 @@
 package com.lambda.primary.Configurations;
 
-import com.lambda.primary.Objects.User.UserConverter;
+import com.lambda.primary.Middlewares.Interceptors.AuthorizationInterceptor;
+import com.lambda.primary.Objects.User.BaseUserConverter;
+import com.lambda.primary.Services.AuthTokenServices;
+import com.lambda.primary.Services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,9 +12,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
+    @Autowired
+    private AuthTokenServices authTokenServices;
+    @Autowired
+    private UserServices userServices;
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new UserConverter());
+        registry.addConverter(new BaseUserConverter(userServices));
     }
 
     //public cors mapping
