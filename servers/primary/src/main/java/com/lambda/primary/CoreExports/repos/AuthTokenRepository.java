c2d810b7f549a8286738ca75c3d92da1b26e8908
joelittyab-git <joeittab@gmail.com>
2023-09-26 21:39:07 +0530

    added queries

--- servers/primary/src/main/java/com/lambda/primary/CoreExports/repos/AuthTokenRepository.java
+++ servers/primary/src/main/java/com/lambda/primary/CoreExports/repos/AuthTokenRepository.java
@@ -1,6 +1,7 @@
 package com.lambda.primary.CoreExports.repos;
 
 import com.lambda.primary.CoreExports.entities.AuthToken;
+import com.lambda.primary.CoreExports.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,6 +9,12 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface AuthTokenRepository extends JpaRepository<AuthToken, Long> {
+
+    @Query(
+            value = "SELECT * FROM authtoken WHERE token=:token",
+            nativeQuery = true
+    )
+    AuthToken queryAuthTokenFromToken(@Param("token") String token);
 
     @Query(
             value = "SELECT * FROM authtoken WHERE token=:token",
