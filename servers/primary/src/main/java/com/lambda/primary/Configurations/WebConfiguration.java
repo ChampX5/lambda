ca9f07b6110107f2e72f7a98997d2217aa02f73e
joelittyab-git <joeittab@gmail.com>
2023-10-05 20:36:40 +0530

    web configs

--- servers/primary/src/main/java/com/lambda/primary/Configurations/WebConfiguration.java
+++ servers/primary/src/main/java/com/lambda/primary/Configurations/WebConfiguration.java
@@ -1,13 +1,18 @@
 package com.lambda.primary.Configurations;
 
 import com.lambda.primary.Middlewares.Interceptors.AuthorizationInterceptor;
+import com.lambda.primary.Middlewares.Interceptors.SecurityInterceptors;
 import com.lambda.primary.Objects.User.BaseUserConverter;
+import com.lambda.primary.Runnables.LoginUpdates;
 import com.lambda.primary.Services.AuthTokenServices;
 import com.lambda.primary.Services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -25,6 +30,6 @@ public class WebConfiguration implements WebMvcConfigurer {
     //public cors mapping
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://loclahost:3000");
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
     }
 }
